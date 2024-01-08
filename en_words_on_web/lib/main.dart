b432276f05be9a442bdb6b126908803e6a3364ab
shunsuke sato <babblemann.shunsee@gmail.com>
2024-01-09 07:45:19 +0900

    ios/androidでUIの出しわけを試す

--- en_words_on_web/lib/main.dart
+++ en_words_on_web/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:en_words_on_web/page/word_list_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -12,23 +13,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: const [
+    //   return MaterialApp(
+    //   localizationsDelegates: const [
+    //     AppLocalizations.delegate,
+    //     GlobalMaterialLocalizations.delegate,
+    //     GlobalWidgetsLocalizations.delegate,
+    //     GlobalCupertinoLocalizations.delegate,
+    //   ],
+    //   supportedLocales: const [
+    //     Locale('ja', ''),
+    //     Locale('en', ''),
+    //   ],
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //     useMaterial3: true,
+    //   ),
+    //   home: const WordListPage(title: '俺の単語帳'),
+    // );
+    return const CupertinoApp(
+      localizationsDelegates: [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
+      supportedLocales: [
         Locale('ja', ''),
         Locale('en', ''),
       ],
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const WordListPage(title: '俺の単語帳'),
+      home: WordListPage(title: '俺の単語帳 3'),
     );
   }
 }
