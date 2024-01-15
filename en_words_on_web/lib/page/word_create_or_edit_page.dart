54c37317e1edb60c0b59c03624bb93d554d9915e
shunsuke sato <babblemann.shunsee@gmail.com>
2024-01-15 22:59:59 +0900

    Formの導入

--- en_words_on_web/lib/page/word_create_or_edit_page.dart
+++ en_words_on_web/lib/page/word_create_or_edit_page.dart
@@ -19,6 +19,8 @@ class _WordCreateOrEditPageState extends State<WordCreateOrEditPage> {
   String _urlString = "";
   // bool _canFinish = false;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -38,72 +40,86 @@ class _WordCreateOrEditPageState extends State<WordCreateOrEditPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.originalWord == null ? '作成' : '編集'),
       ),
-      body: Container(
-          padding: const EdgeInsets.all(16),
-          // TODO: Formで囲むと良い？？
-          // https://qiita.com/kurun_pan/items/3378875ff034614f381a#form
-          // validationの処理などを綺麗に書ける？
-          child: Column(
-            children: [
-              TextFormField(
-                initialValue: _title,
-                decoration: const InputDecoration(hintText: 'Title'),
-                onChanged: (value) {
-                  _title = value;
-                },
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: _description,
-                decoration:
-                    const InputDecoration(hintText: 'Description (optional)'),
-                onChanged: (value) {
-                  _description = value;
-                },
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                initialValue: _urlString,
-                decoration: const InputDecoration(hintText: 'URL (optional)'),
-                onChanged: (value) {
-                  _urlString = value;
-                },
-              ),
-              const SizedBox(height: 24),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: Consumer(builder: (context, ref, child) {
-                  return ElevatedButton(
-                    child: const Text('完了'),
-                    onPressed: () {
-                      if (_title.isEmpty) {
-                        return;
+      body: Form(
+          key: _formKey,
+          child: Container(
+              padding: const EdgeInsets.all(16),
+              // TODO: Formで囲むと良い？？
+              // https://qiita.com/kurun_pan/items/3378875ff034614f381a#form
+              // validationの処理などを綺麗に書ける？
+              child: Column(
+                children: [
+                  TextFormField(
+                    initialValue: _title,
+                    decoration: const InputDecoration(
+                        labelText: '単語', hintText: '例）Apple'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '入力必須項目です!';
                       }
-
-                      String? finalDescription =
-                          _description.isEmpty ? null : _description;
-                      String? finalUrlString =
-                          _urlString.isEmpty ? null : _urlString;
-
-                      final originalWord = widget.originalWord;
-                      if (originalWord == null) {
-                        final word = Word.create(
-                            title: _title,
-                            description: finalDescription,
-                            urlString: finalUrlString);
-                        ref.read(wordListProvider.notifier).add(word: word);
-                      } else {
-                        final word = Word(originalWord.id, _title,
-                            finalDescription, finalUrlString);
-                        ref.read(wordListProvider.notifier).edit(word: word);
-                      }
-                      Navigator.of(context).pop();
+                      return null;
                     },
-                  );
-                }),
-              ),
-            ],
-          )),
+                    onChanged: (value) {
+                      _title = value;
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    initialValue: _description,
+                    decoration: const InputDecoration(
+                        labelText: '説明 (省略可)', hintText: '例）リンゴ'),
+                    onChanged: (value) {
+                      _description = value;
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    initialValue: _urlString,
+                    decoration: const InputDecoration(labelText: 'URL (省略可)'),
+                    onChanged: (value) {
+                      _urlString = value;
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Consumer(builder: (context, ref, child) {
+                      return ElevatedButton(
+                        child: const Text('完了'),
+                        onPressed: () {
+                          if (_formKey.currentState != null &&
+                              _formKey.currentState!.validate()) {
+                            // validate成功
+                          } else {
+                            return;
+                          }
+
+                          String? finalDescription =
+                              _description.isEmpty ? null : _description;
+                          String? finalUrlString =
+                              _urlString.isEmpty ? null : _urlString;
+
+                          final originalWord = widget.originalWord;
+                          if (originalWord == null) {
+                            final word = Word.create(
+                                title: _title,
+                                description: finalDescription,
+                                urlString: finalUrlString);
+                            ref.read(wordListProvider.notifier).add(word: word);
+                          } else {
+                            final word = Word(originalWord.id, _title,
+                                finalDescription, finalUrlString);
+                            ref
+                                .read(wordListProvider.notifier)
+                                .edit(word: word);
+                          }
+                          Navigator.of(context).pop();
+                        },
+                      );
+                    }),
+                  ),
+                ],
+              ))),
     );
   }
 }
